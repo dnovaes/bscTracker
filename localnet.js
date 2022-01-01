66e1ef81fb22f74bf8059ec1803255a49bf5e30c
dnovaes <novaesdigo@gmail.com>
2021-12-31 22:32:27 -0300

    Add method gweiToUSD and clear up web3Custom module

--- localnet.js
+++ localnet.js
@@ -1,25 +1,39 @@
 const interval = require('./interval')
 const web3Custom = require('./web3_custom/methods')
-
 const Web3 = require('web3');
 
 const BscNode = "http://127.0.0.1:8545"
 const web3BSC = new Web3(BscNode)
 
-const myWalletAddr = "0x7345B350143E4048Ad7d9E2962BEEC702bC8af8f" //testNet
-const myWalletPrivateKey = "0x414dcdb89a5f9ca6358a3b7237822724e5dfc39bc5554e89b288ccae0bde176d"
+//testNet
+const myWalletAddr = "0x9f0879Fda1dF24135E29445b12D3e2470f9767Fb"
+const myWalletPrivateKey = "0xf333104694198f68670a16249d73fa9593dc58e13fc74e18128f3e977ecbece3"
 web3BSC.eth.accounts.wallet.add(myWalletPrivateKey)
 
 // startProgram
+async function main () {
+    console.log(`Web3 version: ${Web3.version}\n`)
 
-console.log(Web3.version)
-web3Custom.init(web3BSC)
-web3Custom.getBalance(myWalletAddr)
-web3Custom.getAccounts()
+    web3Custom.init(web3BSC)
+    let beforeBalance = await web3Custom.getBalance(myWalletAddr)
+    console.log(`Initial Balance: ${beforeBalance}`)
+    web3Custom.getAccounts()
 
-web3Custom.transfer(myWalletAddr, myWalletPrivateKey, "0xE5e7c6981675F30c61F8a2cc9AE3aC1FE4f66C78", "0.002")
-    .then(res => {
-        console.log(`Transaction hash: ${res.transactionHash}`)
-        console.log(res)
-        web3Custom.getBalance(myWalletAddr)
-    })
+    let valueToTransfer = "1"
+    console.log(`Transfering ${valueToTransfer} ETH ...`)
+
+    gasPriceGwei = "1000"
+    web3Custom.transfer(myWalletAddr, myWalletPrivateKey, "0xE5e7c6981675F30c61F8a2cc9AE3aC1FE4f66C78", valueToTransfer, gasPriceGwei)
+        .then(res => {
+            console.log(`Transaction hash: ${res.transactionHash}`)
+
+            web3Custom.getBalance(myWalletAddr)
+                .then(currBalance => {
+                    let valueUsed = web3Custom.fromWei((beforeBalance-currBalance).toString())
+                    console.log(`Transfer + GasFee: ${valueUsed} ETH, ${web3Custom.gweiToUSD(valueUsed, "ether", "ether").toFixed(2)} USD`)
+                })
+        })
+
+}
+
+main()

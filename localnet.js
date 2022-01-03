d5f284922cd8d426c0a337e6c1fba8b91653869a
dnovaes <novaesdigo@gmail.com>
2022-01-03 00:41:19 -0300

    Implement reading address from config local files

--- localnet.js
+++ localnet.js
@@ -1,14 +1,17 @@
 const interval = require('./utils/interval')
 const web3Custom = require('./web3_custom/methods')
 const Web3 = require('web3');
+const config = require('./localconfig.json');
 
 const BscNode = "http://127.0.0.1:8545"
 const web3BSC = new Web3(BscNode)
 
-//testNet
+const walletAddr = []
+walletAddr.push(config.walletAddr0)
+walletAddr.push(config.walletAddr1)
 
-const myWalletAddr = "0xe0fB750d66017b06327F905515259a01Ed6feE90"
-const myWalletPrivateKey = "0x821e7d9d8b8c8b5ef7d5f50e823212bbaee8b7686d9a3a292e589312a4346e44"
+const privWalletAddr = []
+privWalletAddr.push(config.privWalletAddr0)
 //web3BSC.eth.accounts.wallet.add(myWalletPrivateKey)
 
 //methods
@@ -16,16 +19,16 @@ async function main () {
     console.log(`Web3 version: ${Web3.version}\n`)
 
     web3Custom.init(web3BSC)
-    let beforeBalance = await web3Custom.getBalance(myWalletAddr)
+    let beforeBalance = await web3Custom.getBalance(walletAddr[0])
     console.log(`Initial Balance: ${web3Custom.fromWei(beforeBalance)} ETH\n`)
 
     let valueToTransfer = "0.001"
     console.log(`Transfering ${valueToTransfer} ETH ...`)
 
     let payload = {
-        fromAddr: myWalletAddr,
-        fromPrivAddr: myWalletPrivateKey,
-        toAddr: "0xE5e7c6981675F30c61F8a2cc9AE3aC1FE4f66C78",
+        fromAddr: walletAddr[0],
+        fromPrivAddr: privWalletAddr[0],
+        toAddr: walletAddr[1],
         valueToTransfer: valueToTransfer,
         gasPriceGwei: "5",
         chainCommon: null
@@ -34,7 +37,7 @@ async function main () {
         .then(res => {
             console.log(`Transaction hash: ${res.transactionHash}`)
 
-            web3Custom.getBalance(myWalletAddr)
+            web3Custom.getBalance(walletAddr[0])
                 .then(currBalance => {
                     let valueUsed = web3Custom.fromWei((beforeBalance-currBalance).toString())
                     console.log(`\nTransfer + GasFee: ${valueUsed} ETH, ${web3Custom.toUSD(valueUsed, "ether", "ether").toFixed(2)} USD`)
